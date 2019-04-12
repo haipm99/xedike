cf3cd78b8beb84de076845f62d988798eb2df3f8
haipm99 <haipmse130332@fpt.edu.vn>
2019-04-12 20:20:41 +0700

    create connect db and model user

---
+++ models/user.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const userSchema = new mongoose.Schema({
+    email: { type: String, required: true },
+    password:{ type: String, required: true },
+    fullname:{ type: String, required: true },
+    phone: { type: String, required: true },
+    dateOfBirth: { type: Date, required: true },
+    registerDate: { type: Date, default: new Date() },
+    numberOfTrips:{ type: Number, required: false },
+    numberOfKms: { type: Number, required: false },
+    avatar: {type:String},
+    isActive: { type: Boolean, default: true },
+});
+
+const user = mongoose.model('User', userSchema);
+
+
+module.exports = {
+    userSchema, user
+}
--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 /* 3rd packages*/
-const express = require('express')
-
+const express = require('express');
+const mongoose = require('mongoose');
 /* project packages */
 
 
+/*connect database */
+mongoose.connect('mongodb://localhost:27017/fs03-xedike',{useNewUrlParser: true})
+.then(console.log("connect to DB Success !"))
+.catch(console.log)
 /* initialize project */
 
 const app = express();
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
 app.listen(port,() => {
     console.log(`listen on port ${port}`)
-})
+});
