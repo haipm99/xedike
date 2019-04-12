b1f90b2e7494a98f7e0f209f3286c600afcba985
haipm99 <haipmse130332@fpt.edu.vn>
2019-04-12 19:49:57 +0700

    initialize server

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-console.log("hello");
+/* 3rd packages*/
+const express = require('express')
+
+/* project packages */
+
+
+/* initialize project */
+
+const app = express();
+const port = process.env.PORT || 5000
+
+app.listen(port,() => {
+    console.log(`listen on port ${port}`)
+})
